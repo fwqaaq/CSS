4486cc09e2efa6c8bbcf9f5e08e5e87025ba61aa
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-07-14 06:11:05 +0800

    read file

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,32 @@
 import { defineConfig } from "vitepress"
+import * as fs from "fs"
+import { fileURLToPath } from "url"
+
+function recurReadFile() {
+  const res = []
+  const dir = fs.readdirSync("./docs")
+  dir.forEach(curDir => {
+    curDir = "../" + curDir
+    const path = fileURLToPath(new URL(curDir, import.meta.url))
+    try {
+      const directory = fs.statSync(path)
+      if (directory.isDirectory() && !curDir.includes(".vitepress")) {
+        const mdDir = { text: curDir.split("/")[1], collapsable: true, items: [] }
+        res.push(mdDir)
+        fs.readdirSync(path).forEach(file => {
+          if (/.*\.md/.test(file)) {
+            const spliceFileName = file.replace(/(\.md)/g, "")
+            mdDir.items.push({ text: spliceFileName, link: `/${curDir.split("/")[1]}/${spliceFileName}` })
+          }
+        })
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  })
+  return res
+}
+
 export default defineConfig({
   title: 'css',
   description: 'css学习',
@@ -28,82 +56,7 @@ export default defineConfig({
           { text: "学前必读", link: "/" }
         ]
       },
-      {
-        text: 'css基础',   // 必要的
-        collapsable: true, // 可选的, 默认值是 true,
-        items: [
-          { text: "css基础", link: "/css/css" },
-          { text: "css选择器", link: "/css/css选择器" },
-          { text: "css属性值语法", link: "/css/css属性值语法" },
-          { text: "css逻辑属性", link: "/css/css逻辑属性" },
-          { text: "css变量及全局关键字", link: "/css/css变量及全局关键字" },
-          { text: "css层叠性", link: "/css/层叠性" },
-          { text: "流的破坏与保护", link: "/css/流的破坏与保护" },
-        ]
-      },
-      {
-        text: 'css布局',   // 必要的
-        collapsable: true, // 可选的, 默认值是 true,
-        items: [
-          { text: "分栏布局", link: "/css布局/分栏布局" },
-          { text: "媒体查询", link: "/css布局/媒体查询" },
-          { text: "图片等多媒体处理", link: "/css布局/图片等多媒体的处理" },
-          { text: "css布局", link: "/css布局/css布局" },
-          { text: "flex", link: "/css布局/flex" },
-          { text: "grid", link: "/css布局/grid" },
-          { text: "shapes", link: "/css布局/shapes" },
-        ]
-      },
-      {
-        text: 'css动画',   // 必要的
-        collapsable: true, // 可选的, 默认值是 true,
-        items: [
-          { text: "CSS动画", link: "/css动画/CSS动画" },
-          { text: "渐变", link: "/css动画/渐变" },
-          { text: "CSS动画transform", link: "/css动画/CSS动画transform" },
-          { text: "CSS过渡", link: "/css动画/CSS过渡" },
-        ]
-      },
-      {
-        text: '视觉效果',   // 必要的
-        collapsable: true, // 可选的, 默认值是 true,
-        items: [
-          { text: "用户体验", link: "/css视觉效果/用户体验" },
-          { text: "图形处理", link: "/css视觉效果/图形处理" },
-          { text: "视觉效果", link: "/css视觉效果/视觉效果" },
-        ]
-      },
-      {
-        text: 'css揭秘',   // 必要的
-        collapsable: true, // 可选的, 默认值是 true,
-        items: [
-          { text: "背景和边框", link: "/css揭秘/背景和边框" },
-          { text: "形状", link: "/css揭秘/形状" },
-          { text: "css字体", link: "/css揭秘/css字体" },
-          { text: "视觉效果", link: "/css揭秘/视觉效果" },
-        ]
-      },
-      {
-        text: 'scss',   // 必要的
-        collapsable: true, // 可选的, 默认值是 true,
-        items: [
-          { text: "相关介绍", link: "/scss/相关介绍" },
-          { text: "环境配置", link: "/scss/环境配置" },
-          { text: "scss语法", link: "/scss/scss语法" },
-        ]
-      },
-      {
-        text: 'css技巧',
-        collapsable: true,
-        items: [
-          { text: "HTML内置行为和应用", link: "/css技巧/HTML内置行为和应用" },
-          { text: "居中", link: "/css技巧/居中" },
-          { text: "用户行为及网站换肤", link: "/css技巧/用户行为及网站换肤" },
-          { text: "layer", link: "/css技巧/layer" },
-          { text: "文字换行", link: "/css技巧/文字换行" },
-          { text: "css声明的优先级", link: "/css技巧/css声明的优先级" },
-        ]
-      }
+      ...recurReadFile()
     ]
   },
 })
